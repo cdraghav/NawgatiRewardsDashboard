26eaf5fb681b8a818ac1319c5cf582c42777dead
cdraghav <raghav@nawgati.com>
2026-04-29 12:26:25 +0530

    fix envs

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { authClient } from "./auth-client";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
